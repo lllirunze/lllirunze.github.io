780447ab0e471f38f3338522f168a41a3e3eb401
lirunze <lirunze.me@gmail.com>
2026-05-02 22:41:32 +0800

    update

--- src/scripts/terminalCommands.js
+++ src/scripts/terminalCommands.js
@@ -1,10 +1,14 @@
 export const commands = {
   help: 
-`help:    Show all available commands
-whoami:  Display my ASCII signature
-skills:  Show my tech stack
-hobbies: Show my hobbies
-clear:   Clear the terminal`,
+`[ help       ] Show all available commands
+[ whoami     ] Display my ASCII signature
+[ skills     ] Show my tech stack
+[ hobbies    ] Show my hobbies
+[ education  ] Show my education
+[ experience ] Show my experience
+[ leadership ] Show leadership experience
+[ clear      ] Clear the terminal`,
+
   whoami: 
 `██████╗ ██╗   ██╗███╗   ██╗███████╗███████╗    ██╗     ██╗
 ██╔══██╗██║   ██║████╗  ██║╚══███╔╝██╔════╝    ██║     ██║
@@ -12,6 +16,45 @@ clear:   Clear the terminal`,
 ██╔══██╗██║   ██║██║╚██╗██║ ███╔╝  ██╔══╝      ██║     ██║
 ██║  ██║╚██████╔╝██║ ╚████║███████╗███████╗    ███████╗██║
 ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚══════╝    ╚══════╝╚═╝`,
+
   skills: "Java | Spring | Kafka | React | Node.js",
-  hobbies: "🏸 Badminton | 🎱 Billiards | 🐺 Werewolf"
+
+  hobbies: "🏸 Badminton | 🎱 Billiards | 🐺 Werewolf",
+
+  education:
+`[Education]
+
+New York University
+- Sep 2023 -- May 2025
+- M.S. in Computer Science
+- GPA: 3.93 / 4.00
+
+Tianjin University
+- Sep 2019 -- Jun 2023
+- B.Eng in Computer Science
+- GPA: 3.66 / 4.00`,
+
+  experience:
+`[Professional Experience]
+
+Huawei Technologies Co., Ltd
+- Software Engineer (Aug 2025 -- Now)
+
+Huawei Technologies Co., Ltd
+- Software Engineer Intern (Jun 2024 -- Aug 2024)
+
+Hangzhou Dingdian Intelligent Computing
+- Software Engineer Intern (Feb 2024 -- May 2024)
+
+Xiaomi Corporation
+- Web Dev Intern (Apr 2022 -- Jun 2022)`,
+
+  leadership:
+`[Leadership Experience]
+
+Student Union
+- Minister of Organization Department (Aug 2020 -- Jun 2021)
+
+Association Committee
+- Director of Social Practice Office (Sep 2020 -- Jun 2021)`
 };
